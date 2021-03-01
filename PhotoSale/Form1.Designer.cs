856fc465e0191a8d4110d3ef5ac3c6f7fa33fdf8
Дмитрий Дорошкевич <doroschkewich11@gmail.com>
2021-03-02 02:26:30 +0500

    Добавьте файлы проекта.

---
+++ PhotoSale/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace PhotoSale
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UserInputPhotoNumber = new System.Windows.Forms.TextBox();
+            this.LabelUserInput = new System.Windows.Forms.Label();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.TotalPriceText = new System.Windows.Forms.Label();
+            this.TotalPrice = new System.Windows.Forms.Label();
+            this.UserInputPhotoFormat = new System.Windows.Forms.ComboBox();
+            this.UserInputPhotoFormatText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UserInputPhotoNumber
+            // 
+            this.UserInputPhotoNumber.Location = new System.Drawing.Point(15, 25);
+            this.UserInputPhotoNumber.MaxLength = 2;
+            this.UserInputPhotoNumber.Name = "UserInputPhotoNumber";
+            this.UserInputPhotoNumber.Size = new System.Drawing.Size(41, 20);
+            this.UserInputPhotoNumber.TabIndex = 0;
+            // 
+            // LabelUserInput
+            // 
+            this.LabelUserInput.AutoSize = true;
+            this.LabelUserInput.Location = new System.Drawing.Point(12, 9);
+            this.LabelUserInput.Name = "LabelUserInput";
+            this.LabelUserInput.Size = new System.Drawing.Size(236, 13);
+            this.LabelUserInput.TabIndex = 1;
+            this.LabelUserInput.Text = "Введите количество фотографий для печати:";
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(12, 145);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(75, 23);
+            this.Calculate.TabIndex = 2;
+            this.Calculate.Text = "Рассчитать";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // TotalPriceText
+            // 
+            this.TotalPriceText.AutoSize = true;
+            this.TotalPriceText.Location = new System.Drawing.Point(12, 171);
+            this.TotalPriceText.Name = "TotalPriceText";
+            this.TotalPriceText.Size = new System.Drawing.Size(40, 13);
+            this.TotalPriceText.TabIndex = 3;
+            this.TotalPriceText.Text = "Итого:";
+            // 
+            // TotalPrice
+            // 
+            this.TotalPrice.AutoSize = true;
+            this.TotalPrice.Location = new System.Drawing.Point(58, 171);
+            this.TotalPrice.Name = "TotalPrice";
+            this.TotalPrice.Size = new System.Drawing.Size(13, 13);
+            this.TotalPrice.TabIndex = 4;
+            this.TotalPrice.Text = "0";
+            // 
+            // UserInputPhotoFormat
+            // 
+            this.UserInputPhotoFormat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.UserInputPhotoFormat.FormattingEnabled = true;
+            this.UserInputPhotoFormat.Location = new System.Drawing.Point(15, 68);
+            this.UserInputPhotoFormat.Name = "UserInputPhotoFormat";
+            this.UserInputPhotoFormat.Size = new System.Drawing.Size(99, 21);
+            this.UserInputPhotoFormat.TabIndex = 5;
+            // 
+            // UserInputPhotoFormatText
+            // 
+            this.UserInputPhotoFormatText.AutoSize = true;
+            this.UserInputPhotoFormatText.Location = new System.Drawing.Point(15, 52);
+            this.UserInputPhotoFormatText.Name = "UserInputPhotoFormatText";
+            this.UserInputPhotoFormatText.Size = new System.Drawing.Size(130, 13);
+            this.UserInputPhotoFormatText.TabIndex = 6;
+            this.UserInputPhotoFormatText.Text = "Выберите формат фото:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 193);
+            this.Controls.Add(this.UserInputPhotoFormatText);
+            this.Controls.Add(this.UserInputPhotoFormat);
+            this.Controls.Add(this.TotalPrice);
+            this.Controls.Add(this.TotalPriceText);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.LabelUserInput);
+            this.Controls.Add(this.UserInputPhotoNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UserInputPhotoNumber;
+        private System.Windows.Forms.Label LabelUserInput;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Label TotalPriceText;
+        private System.Windows.Forms.Label TotalPrice;
+        private System.Windows.Forms.ComboBox UserInputPhotoFormat;
+        private System.Windows.Forms.Label UserInputPhotoFormatText;
+    }
+}
+
---
+++ PhotoSale/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PhotoSale
+{
+    public partial class Form1 : Form
+    {
+        private static float PhotoPrice9x12 = 8.5f, PhotoPrice12x15 = 11.0f, PhotoPrice18x24 = 32.0f;
+        private static int Discount = 10;
+        private static string PhotoText9x12 = $"Фото 9х12", PhotoText12x15 = $"Фото 12х15", PhotoText18x24 = $"Фото 18х24";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UserInputPhotoFormat.Items.Add(PhotoText9x12);
+            UserInputPhotoFormat.Items.Add(PhotoText12x15);
+            UserInputPhotoFormat.Items.Add(PhotoText18x24);
+        }
+
+        private void Main()
+        {
+
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            int UserPhotoNumber = Convert.ToInt32(UserInputPhotoNumber.Text);
+
+            //Рассчёт суммы взависимости от выбора формата фото
+            if (UserInputPhotoFormat.SelectedIndex == 0) //1 элемент, фото 9 на 12
+            {
+                TotalPrice.Text = Convert.ToString(UserPhotoNumber * PhotoPrice9x12) + " руб.";
+            }
+
+            if (UserInputPhotoFormat.SelectedIndex == 1) //2 элемент, фото 12 на 15
+            {
+                TotalPrice.Text = Convert.ToString(UserPhotoNumber * PhotoPrice12x15) + " руб.";
+            }
+
+            if (UserInputPhotoFormat.SelectedIndex == 2) //3 элемент, фото 18 на 24
+            {
+                TotalPrice.Text = Convert.ToString(UserPhotoNumber * PhotoPrice18x24) + " руб.";
+            }
+        }
+    }
+}
